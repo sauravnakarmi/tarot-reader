cc455782d3f19500f9d107d312c7a35f8013371d
sauravnakarmi <sauravnakarmi@gmail.com>
2026-06-12 19:01:12 -0500

    Update server.js
    Health Check Route

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Tarot API is running!");
+});
+
 app.post("/reading", async (req, res) => {
 
     const { cards } = req.body;
